ce6e05206b2f1f2820b0cd2125ae0599be79665c
Aayush <aayushjaiswal0114@gmail.com>
2017-11-19 19:15:16 +0530

    Setup complete

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 let api = require('./server/routes/api.js');
+
+app.set('view engine', 'html');
+app.engine('html', function (path, options, callbacks) {
+  fs.readFile(path, 'utf-8', callback);
+});
+
+app.use(express.static(path.join(__dirname, 'client')));
+
 app.use('/api',api);
 
 
