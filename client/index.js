5716123d969109375c70eb44b1682737b43de14c
aayush1408 <aayushjaiswal0114@gmail.com>
2018-01-17 14:38:44 +0530

    proxy managed

--- client/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './Components/navbar';
-import Full from './Full.js';
-ReactDOM.render(<Full />, document.getElementById('app'));
